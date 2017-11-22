60932ecdbff431ecca9c37dde607b9b1f8b8f239
Weijia <weijia31415@gmail.com>
2017-11-22 08:47:18 -0700

    check in schema files and package update

---
+++ models/wanblock.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose')
+  , Schema = mongoose.Schema;
+ 
+var WanblockSchema = new Schema({
+  number: { type: Number, unique: true, index: true},
+  difficulty: {type: Number},
+  extraData: {type: String},
+  gasLimit: {type: Number},
+  gasUsed: {type: Number},
+  hash: {type: String},
+  logsBloom: {type: String},
+  miner: {type: String},
+  mixHash: {type: String},
+  nonce: {type: String},
+  parentHash: {type: String},
+  receiptsRoot: {type: String},
+  sha3Uncles: {type: String},
+  size: {type: Number, default: 0},
+  stateRoot: {type: String},
+  timestamp: { type: Number, default: 0 },
+  totalDifficulty: {type: Number},
+  transactions: { type: Array, default: [] },
+  transactionsRoot: {type: String},
+  uncles: { type: Array, default: [] }
+
+}, {id: false});
+
+module.exports = mongoose.model('Wanblock', WanblockSchema);
+
---
+++ models/wantx.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose')
+  , Schema = mongoose.Schema;
+ 
+var WanTxSchema = new Schema({
+  hash: { type: String, lowercase: true, unique: true, index: true},
+  blockNumber:{ type: Number, default:0},
+  from: { type: String},
+  to: { type: String},
+  gas: { type: Number, default: 0 },
+  gasPrice: { type: String},
+  timestamp: { type: Number, default: 0 },
+  blockHash: { type: String },
+  nonce:{type:Number, default:0},
+  value: {type: String},
+  txtype: {type: String},
+  transactionIndex: {type:Number, default: 0},
+}, {id: false});
+
+module.exports = mongoose.model('Wantx', WanTxSchema);

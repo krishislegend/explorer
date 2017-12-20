410f8d997e6b87fe381403611b25b83a1cd45448
Jason He <hejian_best@163.com>
2017-12-21 00:51:59 +0800

    fix searchbar submit event

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,21 +1,34 @@
-window.onload=function() {
-  var go=document.getElementById("goSearch");
-  go.addEventListener('click',function(e) {
-    var inp=document.getElementById('input_text');
-    var val=inp.value;
-    if(/^[0-9]{1,10}$/.test(val)){
-      window.open("/block/"+val);
-    }else if(val.length===66&&/^[0][x]/.test(val)){
+window.onload = function() {
+  var go = document.getElementById("goSearch");
+  var form = document.getElementById("search_bar");
+  go.addEventListener('click', function(e) {
+    var inp = document.getElementById('input_text');
+    var val = inp.value;
+    if (/^[0-9]{1,10}$/.test(val)) {
+      window.open("/block/" + val);
+    } else if (val.length === 66 && /^[0][x]/.test(val)) {
       window.open(`/block/trans/${val}`);
-    }else if(val.length===42&&/^[0][x]/.test(val)){
+    } else if (val.length === 42 && /^[0][x]/.test(val)) {
       window.open(`/block/addr/${val}`);
-    }else{
-      trigger('invalid',inp);
+    } else {
+      trigger('invalid', inp);
     }
+    inp.value = "";
   });
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+  })
 }
-function trigger(type,doc) {
-  var eventObj=document.createEvent('HTMLEvents');
-  eventObj.initEvent(type,true,true);
+function trigger(type, doc) {
+  var eventObj = document.createEvent('HTMLEvents');
+  eventObj.initEvent(type, true, true);
   doc.dispatchEvent(eventObj);
+}
+function clickA(url) {
+  var el = document.createElement("a");
+  document.body.appendChild(el);
+  el.href = url;
+  el.target = '_new';
+  el.click();
+  document.body.removeChild(el);
 }
